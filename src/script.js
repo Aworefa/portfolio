f0435b4ad3a81c7cb3397959744f1bd31be414c3
Aworefa <aworefatobi1999@gmail.com>
2022-08-03 20:11:33 +0800

    my first push of my portfolio

---
+++ src/script.js
@@ -0,0 +1,26 @@
+var btn= document.getElementById('menu-btn');
+var nav= document.getElementById('menu');
+btn.addEventListener('click', () =>{
+    btn.classList.toggle('open')
+    nav.classList.add('flex')
+    nav.classList.toggle('hidden')
+})
+
+document.querySelector('#menu').addEventListener('click', () =>{
+    nav.classList.toggle('hidden')
+    btn.classList.remove('open')
+})
+document.querySelector('#deve').addEventListener('mouseover', deve)
+function deve(){
+    document.querySelector('#newword').innerHTML = 'Creating high-quality user interfaces utilizing well-known technologies like HTML, CSS, and JavaScript while promoting improved website interaction'
+}
+
+document.querySelector('#devetwo').addEventListener('mouseover', devenow)
+function devenow(){
+    document.querySelector('#newwordtwo').innerHTML = 'Making quality mobile applications with well-known technologies like dart and flutter and improving mobile applications'
+}
+document.querySelector('#devethree').addEventListener('mouseover', devethree)
+function devethree(){
+    document.querySelector('#newwordthree').innerHTML = 'Coming...'
+    
+}
