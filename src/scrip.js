4074158516f580476f98ce86454bd20179ea0d3d
“Aworefa” <aworefatobi1999@gmail.com>
2023-06-10 03:40:33 -0400

    june update

--- src/scrip.js
+++ src/scrip.js
@@ -1,6 +1,7 @@
-setTimeout(() => {
-  document.querySelector("#loading").style.display = "none";
-}, 6000);
+// setTimeout(() => {
+//   document.querySelector(".loading").classList.add('hidden')
+//   document.querySelector(".body").classList.remove('hidden')
+// }, 5000);
 
 var btn= document.getElementById('bar');
 var nav= document.getElementById('menu');
@@ -15,8 +16,23 @@ document.querySelector('#menu').addEventListener('click', () =>{
     btn.classList.remove('open')
 })
 var typing=new Typed(".info", {
-    strings: ["Frontend web developer","3d Modeler","Industrial Designer","Web Developer"],
+    strings: ["Web Developer", "Aworefa Tobi","Frontend developer","3d Modeler","Industrial Designer",],
     typeSpeed: 100,
     backSpeed: 40,
     loop: true,
 });
+var current = new Date().getFullYear()
+document.querySelector('#yearsofexperience').innerHTML = current - 2021
+console.log(current)
+let thisDay = new Date()
+let day = thisDay.getDay()
+let month = thisDay.getMonth()
+var nameOfDays = ['Sunday','Monday','Tuesday','Wednesday', 'Thursday', 'Friday','Saturday','Sunday']
+var nameOfMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+
+
+document.querySelector('#dayname').innerHTML = nameOfDays[day] + ':'
+document.querySelector('#month').innerHTML = nameOfMonth[month] + ','
+document.querySelector('#day').innerHTML = thisDay.getDate()
+document.querySelector('#year').innerHTML = new Date().getFullYear()
